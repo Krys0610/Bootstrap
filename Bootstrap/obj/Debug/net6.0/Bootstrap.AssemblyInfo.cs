7d82140a08e6678924b875d2dc7d4a55a09af705
Krys0610 <nahid0610@gmail.com>
2024-04-10 14:28:22 -0600

    tabla

--- Bootstrap/obj/Debug/net6.0/Bootstrap.AssemblyInfo.cs
+++ Bootstrap/obj/Debug/net6.0/Bootstrap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d55ba83620ba5d1f18049cdcecacb203ae86ebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
