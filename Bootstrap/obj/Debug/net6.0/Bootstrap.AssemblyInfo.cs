7fcd15432d055656d088a319728fe8a01012a9e2
Krys0610 <nahid0610@gmail.com>
2024-04-11 16:26:11 -0600

    formularios

--- Bootstrap/obj/Debug/net6.0/Bootstrap.AssemblyInfo.cs
+++ Bootstrap/obj/Debug/net6.0/Bootstrap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d198f367af434540a996bc45d220a7d19c90325")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471fcff1f4e0ca6f9922162836cf032905b628a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bootstrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
